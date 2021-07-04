9b75bf4009bf874380b6f0c78c1bbd9a67bfa302
M. Farid Aqsha Ramadhan <farid20ti@mahasiswa.pcr.ac.id>
2021-07-04 13:02:34 +0700

    Initialize

---
+++ src/ppbo_git_farid/Ppbo_git_farid.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ppbo_git_farid;
+
+/**
+ *
+ * @author LEGION
+ */
+public class Ppbo_git_farid {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
